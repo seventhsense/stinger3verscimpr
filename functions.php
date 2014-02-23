f7d764d52d1280d964ee5cd024b15d963f3e4375
seventhsense <seventh@scimpr.com>
2014-02-23 23:31:03 +0900

    add gitignore and improve sns button

--- functions.php
+++ functions.php
@@ -10,4 +10,29 @@ add_theme_support( 'custom-header', $args );
 
 // カスタムサムネイルサイズ
 add_image_size('small_size', 100, 100, false);
+
+// SNSボタンカスタマイズ
+function countTweetNum($url){//ツイート数取得
+    $get_twitter = 'http://urls.api.twitter.com/1/urls/count.json?url=' . $url;
+    $json = file_get_contents($get_twitter);
+    $json = json_decode($json);
+    $twitter = $json->{'count'};
+    print("$twitter");
+}
+function countFbNum($url){//facebook(いいね! + シェア)数取得
+    $get_facebook = 'http://api.facebook.com/restserver.php?method=links.getStats&urls=' . $url;
+    $xml = file_get_contents($get_facebook);
+    $xml = simplexml_load_string($xml);
+    $facebook = $xml->link_stat->total_count; //いいね！のみ…like_count、shareのみ …share_count
+    print("$facebook");
+}
+function countHatebuNum($url){//はてブ数取得
+    $get_hatebu = 'http://api.b.st-hatena.com/entry.count?url=' . $url;
+    $hatebu = file_get_contents($get_hatebu);
+    if($hatebu == ""){$hatebu = 0;}
+    print("$hatebu");
+}
+
+
+
 ?>
